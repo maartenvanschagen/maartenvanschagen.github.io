ed76bdce47d237e36b235a5fd88a8f9de64fa310
maartenvanschagen <maarten@marcovanschagen.nl>
2020-07-22 18:25:28 +0200

    restructure

--- new/standard/README.md
+++
@@ -1,8 +0,0 @@
-# Portfolio
-My portfolio, WIP
-
-## Technologies Used
-This project makes use of the following technologies:
-- HTML5
-- SCSS
-- JS
--- old/functions.js
+++
@@ -1,6 +0,0 @@
-function toggleNavbar(element){
-  element.parentNode.classList.toggle('opened');
-  for(let child of element.parentNode.children){
-    child.classList.toggle('opened');
-  }
-}
